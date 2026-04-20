72c113ff740d0165c94a540cc336419eaaea495a
Atheer Sadoon <nomialhadad@gmail.com>
2026-04-20 23:32:26 +0100

    feat: add DELETE endpoint for users

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -29,7 +29,22 @@ const createUser = async (req, res) => {
   }
 };
 
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete user" });
+  }
+};
+
 module.exports = {
   getUsers,
-  createUser
+  createUser,
+  deleteUser
 };
